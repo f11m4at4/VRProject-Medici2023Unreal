57cdc71e878c9eb646545733b41da0bf7ab09dae
Wonseok Park <jacobyi0730@gmail.com>
2023-02-27 16:20:01 +0900

    Initial commit

---
+++ Source/VRProject/Private/VRPlayer.cpp
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VRPlayer.h"
+#include "EnhancedInputSubsystems.h"
+#include "VRGameModeBase.h"
+#include "Engine/LocalPlayer.h"
+#include "EnhancedInputComponent.h"
+
+// Sets default values
+AVRPlayer::AVRPlayer()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AVRPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Enhanced Input 사용처리
+	auto PC = Cast<APlayerController>( GetWorld()->GetFirstPlayerController() );
+
+	if(PC)
+	{
+		// LocalPlayer
+		auto localPlayer = PC->GetLocalPlayer();
+		auto subSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(localPlayer);
+		if(subSystem)
+		{
+			subSystem->AddMappingContext(IMC_VRInput, 0);
+		}
+	}
+}
+
+// Called every frame
+void AVRPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AVRPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	auto InputSystem = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+	if(InputSystem)
+	{
+		// Binding
+		InputSystem->BindAction(IA_Move, ETriggerEvent::Triggered, this, &AVRPlayer::Move);
+	}
+}
+
+void AVRPlayer::Move(const FInputActionValue& Values)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Move !!!!"));
+	// 사용자의 입력에따라 앞뒤좌우로 이동하고 싶다.
+	// 1. 사용자의 입력에 따라
+	FVector2D Axis = Values.Get<FVector2D>();
+	// 2. 앞뒤좌우라는 방향이 필요.
+	FVector Dir(Axis.X, Axis.Y, 0);
+	// 3. 이동하고싶다.
+	// P = P0 + vt
+	FVector P0 = GetActorLocation();
+	FVector vt = Dir * moveSpeed * GetWorld()->DeltaTimeSeconds;
+	FVector P = P0 + vt;
+	SetActorLocation(P);
+}
+
---
+++ Source/VRProject/Public/VRPlayer.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "VRPlayer.generated.h"
+
+// 사용자의 입력에따라 앞뒤좌우로 이동하고 싶다.
+// 필요속성 : 이동속도, 입력액션, 입력매핑컨텍스트
+UCLASS()
+class VRPROJECT_API AVRPlayer : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AVRPlayer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// 필요속성 : 이동속도, 입력액션, 입력매핑컨텍스트
+	UPROPERTY(EditDefaultsOnly, Category="Input")
+	float moveSpeed = 500;
+	// Input Mapping Context
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputMappingContext* IMC_VRInput;
+	// Input Action for Move
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* IA_Move;
+
+	// 이동처리 함수
+	void Move(const FInputActionValue& Values);
+};
